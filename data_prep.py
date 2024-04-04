db9742bf6cf97276f6510229ba8e3bc48ef263eb
git3st <snochowskiwiktor@gmail.com>
2024-04-04 14:31:50 +0200

    added Dataset class with generic methods + initial main commit

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,35 @@
 import os
 import pandas as pd
 
-dataset_path = os.path.join(os.path.curdir, 'datasets', 'ObesityDataSet.csv')
+
+class Dataset:
+    def __init__(self, filename, train=0.8, test=0.15, validation=0.05):
+        self.full_dataset_path = os.path.join(os.path.curdir, 'datasets', filename)
+        self.full_dataset = pd.read_csv(self.full_dataset_path)
+        self.train_set = pd.DataFrame
+        self.validate_set = pd.DataFrame
+        self.test_set = pd.DataFrame
+
+    def clean_missing_vals(self):
+        self.full_dataset.dropna(axis=0, inplace=True)
+
+    def fill_missing_vals(self):
+        for column in self.full_dataset.columns:
+            self.full_dataset[column] = self.full_dataset[column].fillna().mean()
+
+    def clean_outliers(self):
+        q1 = self.full_dataset.quantile(0.25)
+        q3 = self.full_dataset.quantile(0.75)
+        iqr = q3-q1
+        outliers = (self.full_dataset < (q1 - 1.5 * iqr)) | (self.full_dataset > (q3 + 1.5 * iqr))
+        self.full_dataset = self.full_dataset[~outliers.any(axis=0)]
+
+    def remove_columns(self, cols_to_remove):
+        self.full_dataset.drop(cols_to_remove)
+
+    def normalize(self, cols_to_normalize):
+        for column in self.full_dataset.columns:
+            if column in cols_to_normalize:
+                self.full_dataset[column] = self.full_dataset[column]/self.full_dataset[column].abs().max()
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+from data_prep import Dataset
+
+
+# prepare dataset
+dataset = Dataset('ObesityDataSet.csv')
+cols_to_normalize = []
+cols_to_remove = []
+dataset.remove_columns(cols_to_remove)
+dataset.normalize(cols_to_normalize)
+
+# Use following for train/test/validate
+# dataset.full_dataset
+# dataset.train_set
+# dataset.test_set
+# dataset.validate_set
+
